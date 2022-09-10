efdee82cdd4d2941df5fe87764f5fb67ce56a752
Sushant0999 <sushantraj215@gmail.com>
2022-09-10 20:13:19 +0530

    Merge remote-tracking branch 'origin/master'

--- Fake_Password.java
+++ Fake_Password.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Fake_Password {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
         int t = s.nextInt();
         s.nextLine();
@@ -19,20 +19,39 @@ public class Fake_Password {
     }
 
     private static void password(String original, String fake) {
-        char first = '0';
-        char[] temp = fake.toCharArray();
-        for (int j = 0; j < 2; j++) {
-            first = temp[0];
-            for (int i = 0; i < fake.length() - 1; i++) {
-                temp[i] = temp[i + 1];
-            }
-            temp[temp.length - 1] = first;
-        }
-        fake = String.copyValueOf(temp);
-        if (fake.compareTo(original) != 0){
-            System.out.println("No");
-        }else{
+//        char first = '0';
+//        char[] temp = fake.toCharArray();
+//        for (int j = 0; j < 2; j++) {
+//            first = temp[0];
+//            for (int i = 0; i < fake.length() - 1; i++) {
+//                temp[i] = temp[i + 1];
+//            }
+//            temp[temp.length - 1] = first;
+//        }
+//        fake = String.copyValueOf(temp);
+//        if (fake.compareTo(original) != 0){
+//            System.out.println("No");
+//        }else{
+//            System.out.println("Yes");
+//        }
+        int start = 0;
+        int last = fake.length() - 2;
+        int length = fake.length();
+        int front = length - last;
+        String first = fake.substring(start, front);
+        String firstHalf = fake.substring(front, length);
+        String second = fake.substring(last, length);
+        String secondHalf = fake.substring(start, last);
+        String temp1 = second.concat(secondHalf);
+        String temp2 = firstHalf.concat(first);
+        System.out.println(temp1);
+        System.out.println(temp2);
+        if (temp1.compareTo(original) == 0 || temp2.compareTo(original) == 0) {
             System.out.println("Yes");
+        } else {
+            System.out.println("No");
         }
     }
 }
+//string
+//ngstri
