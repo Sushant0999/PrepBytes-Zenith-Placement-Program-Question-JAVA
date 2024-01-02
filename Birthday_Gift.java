b76edc6ec46eda96d63b2e873f5be8e2ff4b3259
Sushant Raj <66193982+Sushant0999@users.noreply.github.com>
2024-01-02 20:18:57 +0530

    EASY

--- Birthday_Gift.java
+++ Birthday_Gift.java
@@ -1,62 +1,33 @@
 package com.company;
 
-import java.util.Scanner;
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      
+      //write your code here
+      Scanner s = new Scanner(System.in);
+      
+      int t = s.nextInt();
+      
+      while(t-- > 0){
+        int A = s.nextInt();
+        int B = s.nextInt();
+        int C = s.nextInt();
 
-public class Birthday_Gift {
-    static String gift(int a, int b, int c) {
-        if(a >= 0 && b >= 0 && c >= 0){
-            while(a <= b){
-                if(c == 0 && a != b){
-                    break;
-                }
-                if(a == b || b % a == 0){
-
-                    return "YES";
-                }else{
-                    a = a + c;
-                }
-            }
-        }else{
-            if(a >= b){
-                while(a >= b){
-                    if(c == 0 && a != b){
-                        break;
-                    }
-                    if(a == b || b % a == 0){
-
-                        return "YES";
-                    }else{
-                        a = a + c;
-                    }
-                }
-            }else{
-                while(b >= a){
-                    if(c == 0 && a != b){
-                        break;
-                    }
-                    if(a == b || b % a == 0){
-
-                        return "YES";
-                    }else{
-                        a = a + c;
-                    }
-                }
-            }
+        // Check if B can be reached from A using C
+        if (C != 0 && (B - A) % C == 0 && (B - A) / C >= 0) {
+            System.out.println("YES");
+        } else if (A == B) {
+            // Handle the case when A is already equal to B
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
         }
+       }
+      }
 
-        return "NO";
-    }
-
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int t = s.nextInt();
-
-        while (t != 0) {
-            int a = s.nextInt(), b = s.nextInt(), c = s.nextInt();
-            System.out.println(gift(a, b, c));
-            t--;
-        }
-
-
-    }
-}
+        // Input A, B, and C
+        
+  }
